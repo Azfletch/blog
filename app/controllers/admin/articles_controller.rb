f1ac51808d4f8d5207ec6a524274029d97930ad5
Arron Fletcher <a.fletcher1992@gmail.com>
2018-06-01 16:23:34 +0100

    moved all scss imports into application.scss and deleted main.scss. Added base controller. Changed font to sans-serif

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,6 +1,4 @@
-class Admin::ArticlesController < ApplicationController
-
-  http_basic_authenticate_with name: "admin", password: "password", except: [:index, :show]
+class Admin::ArticlesController < Admin::BaseController
 
   def index
     @articles = Article.all
