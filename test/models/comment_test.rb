17a65595ff8c4ea04de535d0532bc413439019a3
Arron Fletcher <a.fletcher1992@gmail.com>
2018-08-03 10:15:05 +0100

    fixed routing on nav-bar

--- test/models/comment_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class CommentTest < ActiveSupport::TestCase
-  test "should not save comments with text in body" do
-    comment = Comment.new
-    assert_not_nil comment.save
-  end
-end
