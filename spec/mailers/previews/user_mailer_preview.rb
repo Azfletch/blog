bcfc3c81964cc733effb86d4bc639514a86d3dd0
Arron Fletcher <a.fletcher1992@gmail.com>
2018-09-28 16:05:01 +0100

    deleted further unused 'User' files

--- spec/mailers/previews/user_mailer_preview.rb
+++
@@ -1,6 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
-class UserMailerPreview < ActionMailer::Preview
-  def user_email_preview
-    UserMailer.with(user: User.first).user_email
-  end
-end
