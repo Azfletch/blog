82f4ee151c4ab93440ab42769826e64a3857d59d
Arron Fletcher <a.fletcher1992@gmail.com>
2018-10-19 15:53:56 +0100

    added grid to c-admin-articles

--- spec/mailers/notification_mailer_spec.rb
+++ spec/mailers/notification_mailer_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe Notifications, :type => :mailer do
 
     it "renders the headers" do
       expect(mail.subject).to eq("New blog post!!!")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["notifications@example.com"])
+      expect(mail.to).to eq([n.email])
+      expect(mail.from).to eq("notifications@example.com")
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).to match("A new article has been posted on Arrons blog!")
     end
   end
 end
