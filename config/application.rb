3be7ecaf361e12494729bf455d8879e6c95bc702
Arron Fletcher <a.fletcher1992@gmail.com>
2018-11-23 11:27:43 +0000

    updated smtp settings and .env

--- config/application.rb
+++ config/application.rb
@@ -15,9 +15,18 @@ module Blog
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.action_mailer.default_url_options = { host: "localhost:4000" }
-    config.action_mailer.asset_host = "http://localhost:4000"
-    config.action_mailer.delivery_method = :postmark
-    config.action_mailer.postmark_settings = { :api_token => "POSTMARK-SERVER-API-TOKEN-HERE" }
+
+    # Not sure if I need this line as method already set in config/environments/development.rb
+    config.action_mailer.delivery_method = :smtp
+
+    # implemented settings based on https://postmarkapp.com/developer/user-guide/sending-email/sending-with-smtp
+    config.action_mailer.postmark_settings = {
+      address:                "smtp.postmarkapp.com",
+      port:                   25,
+      user_name:              ENV.fetch("SMTP_USERNAME"),
+      password:               ENV.fetch("SMTP_PASSWORD"),
+      authentication:         "plain",
+      enable_starttls_auto:   true
+    }
   end
 end
